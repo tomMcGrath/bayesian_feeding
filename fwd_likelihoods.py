3db4643f65fadf177ee60d27cc61b4caa7a6706a
tomMcGrath <thomas.m.mcgrath@gmail.com>
2017-10-11 16:08:22 +0100

    New long pause model works - commit before overwriting

--- fwd_likelihoods.py
+++ fwd_likelihoods.py
@@ -37,6 +37,21 @@ def phi_L(t, x0, k1, theta8, theta9):
 
     return ans
 
+def phi_L_new(t, x0, k1, theta7, theta8):
+    if x0 > 0.0:
+        t_c = 2.0*np.sqrt(x0)/k1 # time to hit zero
+    else:
+        t_c = 0.0
+
+    if t >= t_c:
+        x = 0.0
+    else:
+        x = 0.25*np.power((2.*np.sqrt(x0) - k1*t), 2)
+
+    ans = np.power(1./(theta7 + theta8*x), 2)
+
+    return ans
+
 def Q(x, theta5, theta6):
     eps = 0.01
     ans = eps + (1. - 2.*eps)/(1. + np.exp(-0.1*theta5*(x-20.*theta6)))
@@ -58,8 +73,24 @@ def psi_L(t, x0, k1, theta8, theta9):
     ans = integrate.quad(phi_L, 0, t, args=(x0, k1, theta8, theta9))[0]
     return ans
 
+def psi_L_new(t, x0, k1, theta7, theta8):
+    ans = integrate.quad(phi_L_new, 0, t, args=(x0, k1, theta7, theta8))[0]
+    return ans
+
 def L_an_int(t, x0, k1, theta7, theta8):
     return 2.*np.arctan(0.5*np.sqrt(theta8/theta7)*(k1*t - 2.*np.sqrt(x0)))/(k1*np.sqrt(theta7*theta8))
+
+def L_an_int_new(t, x0, k1, theta7, theta8):
+    x = 0.25*np.power(k1*t, 2) - k1*t*np.sqrt(x0) + x0
+
+    part1 = k1*t*theta8*np.power(theta7, 2)*(0.5*theta7 + theta8*(0.25*k1*t*np.sqrt(x0) - 0.5*x0))
+    part2_1 = theta7*(np.power(theta7, 2) + theta7*theta8*(x + x0) + x0*np.power(theta8, 2)*x)
+    part2_2 = np.sqrt(theta7*theta8)*(np.arctan((0.5*k1*t - np.sqrt(x0))*np.sqrt(theta8/theta7)) + np.arctan(np.sqrt(theta8*x0/theta7)))
+    
+    denom = k1*np.power(theta7, 3)*theta8*(theta7 + x0*theta8)*(theta7 + theta8*x)
+
+    ans = (part1 + part2_1*part2_2)/denom
+    return ans
 
 def psi_L_an(t, x0, k1, theta7, theta8):
     t_c = 2.*np.sqrt(x0)/k1
@@ -69,6 +100,15 @@ def psi_L_an(t, x0, k1, theta7, theta8):
 
     else:
         return psi_L_an(t_c, x0, k1, theta7, theta8) + (t - t_c)/theta7
+
+def psi_L_an_new(t, x0, k1, theta7, theta8):
+    t_c = 2.*np.sqrt(x0)/k1
+
+    if t <= t_c:
+        return L_an_int_new(t, x0, k1, theta7, theta8)
+
+    else:
+        return psi_L_an_new(t_c, x0, k1, theta7, theta8) + (t - t_c)/np.power(theta7, 2)
 
 """
 CDFs and inversions for sampling
@@ -93,6 +133,12 @@ def PDF_L(t, x0, k1, theta8, theta9):
     psi = psi_L_an(t, x0, k1, theta8, theta9)
     phi = phi(t, x0, k1, theta8, theta9)
     ans = phi*np.exp(-psi)
+    return ans
+
+def ll_new(t, x0, k1, theta7, theta8):
+    psi = psi_L_an_new(t, x0, k1, theta7, theta8)
+    phi = phi_L_new(t, x0, k1, theta7, theta8)
+    ans = np.log(phi) - psi
     return ans
 
 def CDF_inv_F(u, x0, rate, theta1):
@@ -123,6 +169,18 @@ def CDF_inv_L_an(u, x0, k1, theta7, theta8):
 
     return ans
 
+def CDF_inv_L_new(u, x0, k1, theta7, theta8):
+    def f_to_min(t, u, x0, k1, theta7, theta8):
+        ans = np.log(1-u) + psi_L_an_new(t, x0, k1, theta7, theta8)
+        return ans
+
+    try:
+        ans = optimize.brentq(f_to_min, 0.0, 1e12, args=(u, x0, k1, theta7, theta8))
+    except RuntimeError:
+        ans = optimize.brentq(f_to_min, 0.0, 1e14, maxiter=1000, args=(u, x0, k1, theta7, theta8))
+
+    return ans
+
 def sample_F(x0, rate, theta1):
     u = np.random.uniform(0,1)
     ans = CDF_inv_F(u, x0, rate, theta1)
@@ -131,6 +189,11 @@ def sample_F(x0, rate, theta1):
 def sample_L(x0, k1, theta8, theta9):
     u = np.random.uniform(0,1)
     ans = CDF_inv_L_an(u, x0, k1, theta8, theta9)
+    return ans
+
+def sample_L_new(x0, k1, theta8, theta9):
+    u = np.random.uniform(0,1)
+    ans = CDF_inv_L_new(u, x0, k1, theta8, theta9)
     return ans
 
 """
